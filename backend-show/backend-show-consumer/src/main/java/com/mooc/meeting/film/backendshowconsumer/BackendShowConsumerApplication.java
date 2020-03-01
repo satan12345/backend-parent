265f9eacf3ef06289a5a0bb70ebe95e1b1e17abc
jipeng <jipeng@able-elec.com>
2020-03-01 20:00:51 +0800

    增加测试用的provider 与consumer

--- backend-show/backend-show-consumer/src/main/java/com/mooc/meeting/film/backendshowconsumer/BackendShowConsumerApplication.java
+++ backend-show/backend-show-consumer/src/main/java/com/mooc/meeting/film/backendshowconsumer/BackendShowConsumerApplication.java
@@ -2,8 +2,14 @@ package com.mooc.meeting.film.backendshowconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//discoveryClient 可以集成大部分注册中心
+@EnableDiscoveryClient
+// 只对eureka使用
+///@EnableEurekaClient
 public class BackendShowConsumerApplication {
 
     public static void main(String[] args) {
