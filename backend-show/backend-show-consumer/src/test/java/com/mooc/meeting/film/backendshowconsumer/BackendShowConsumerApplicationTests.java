ba55db188fe4b55397a0da12ee7096453930bf88
jipeng <jipeng@able-elec.com>
2020-03-01 16:53:52 +0800

    演示模块增加

---
+++ backend-show/backend-show-consumer/src/test/java/com/mooc/meeting/film/backendshowconsumer/BackendShowConsumerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mooc.meeting.film.backendshowconsumer;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShowConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
