719cfcf9f000181bac565afa336ec2813c645154
jipeng <jipeng@able-elec.com>
2020-03-04 19:33:39 +0800

    相关接口开发

---
+++ backend-film/src/main/java/com/mooc/meeting/film/backendfilm/vo/DescribeActorsRespVO.java
@@ -0,0 +1,16 @@
+package com.mooc.meeting.film.backendfilm.vo;
+
+import lombok.Data;
+
+/**
+ * @param
+ * @author jipeng
+ * @date 2020-03-04 17:30
+ */
+@Data
+public class DescribeActorsRespVO {
+
+    private Integer actorId;
+    private String actorName;
+}
+
